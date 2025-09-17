e81d01277864fd73c05b7895f6eb9c74f434fdbb
Aditya Pise <101938972+adipise5@users.noreply.github.com>
2025-09-17 18:42:43 +0530

    Create ArrayString.java

---
+++ Step 1: Learn the basics / Lecture 1: Things to know in JAVA/ArrayString.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Aditya";
+        String s2 = "Pise";      
+        int len = s1.length(); // length() method
+        System.out.println("Length of string: " + len);
+      
+        char c = s1.charAt(2); //find char at position p
+        System.out.println(c);
+      
+        boolean a  = s1.equals(s2); // string comparison
+        System.out.println(a);
+    }
+}
