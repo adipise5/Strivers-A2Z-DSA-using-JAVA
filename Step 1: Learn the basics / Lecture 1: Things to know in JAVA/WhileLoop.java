338dd78e41aea399803618179d2919523f96a2e5
Aditya Pise <101938972+adipise5@users.noreply.github.com>
2025-09-18 07:17:26 +0530

    Create WhileLoop.java

---
+++ Step 1: Learn the basics / Lecture 1: Things to know in JAVA/WhileLoop.java
@@ -0,0 +1,20 @@
+public class Main{
+  public static void main(String[] args){
+    int i = 0;
+      while(i<5){
+        System.out.println(i +"th time");
+        i++;
+      }
+  }
+}
+
+
+// do-while loop - (here do block runs at least one time)
+
+do{
+  //exp
+} while(condition);
+
+//optilization 
+break - exit the loop when the target is found
+continue - skip
