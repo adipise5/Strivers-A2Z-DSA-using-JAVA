ed71d425bd978ca163839fa0508aa142323693b5
Aditya Pise <101938972+adipise5@users.noreply.github.com>
2025-09-19 15:30:49 +0530

    Create Star_Pyramid.java

---
+++ Step 1: Learn the basics / Lecture 1: Things to know in JAVA/Patterns/Star_Pyramid.java
@@ -0,0 +1,21 @@
+/*
+
+*/
+
+
+class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int i,j,k;
+        
+        for(i=1; i<=n; i++){
+            for(j=1; j<= n-i; j++){
+                System.out.print(" ");
+                }
+                 for(k=1; k<= 2*i - 1; k++){
+                    System.out.print("* ");
+               }
+               System.out.println();
+            }
+        }
+    }
